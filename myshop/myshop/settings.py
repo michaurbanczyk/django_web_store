1abe6b0c5be06be2d91cc3ecae6d3ea37e42b3ea
Michal Urbanczyk <michal.urbanczyk@aptiv.com>
2021-03-28 17:35:07 +0200

    Thumbnails added

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shop.apps.ShopConfig'
+    'shop.apps.ShopConfig',
+    'easy_thumbnails'
 ]
 
 MIDDLEWARE = [
